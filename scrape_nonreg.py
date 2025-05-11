bc6e648ca9a01159d23b4ccac6a2ab4ebdf5c7b2
InnovativeOptics <rachdyannaufal@gmail.com>
2025-05-11 16:22:17 +0700

    add scrape_nonreg

---
+++ scrape_nonreg.py
@@ -0,0 +1,361 @@
+from seleniumbase import SB
+from bs4 import BeautifulSoup
+import pandas as pd
+import telegram
+from utils.telegram_utils import send_nonreg_summary_message, \
+    send_non_reg_message
+import asyncio
+from utils.scraping_utils import get_individual_stock
+from dotenv import load_dotenv
+import os
+from pydrive2.auth import GoogleAuth
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+from utils.gsheet_utils import export_to_sheets
+from datetime import datetime as dt
+import pytz
+
+load_dotenv(override=True)
+
+user = os.environ['PROXY_USER']
+password = os.environ['PROXY_PASSWORD']
+proxy_host = os.environ['PROXY_HOST']
+proxy_port = os.environ['PROXY_PORT']
+
+proxy_string = f"{user}:{password}@{proxy_host}:{proxy_port}"
+
+website = os.environ['WEBSITE']
+site_email = os.environ['SITE_EMAIL']
+site_password = os.environ['SITE_PASSWORD']
+
+stock_website = os.environ['STOCK_WEBSITE']
+
+raw_today_data = dt.now(pytz.timezone('Asia/Jakarta'))
+today_date = raw_today_data.strftime("%Y-%m-%d")
+# today_date = '2025-05-09'
+today_month_year = raw_today_data.strftime("%b %Y")
+today_date
+
+if __name__ == "__main__":
+    with SB(uc=True, headless=False, xvfb=True,
+            proxy=proxy_string,
+            maximize=True,
+            ) as sb:
+        sb.driver.execute_cdp_cmd(
+                "Network.setExtraHTTPHeaders",
+                {
+                    "headers": {
+                        'Accept': 'text/html,application/xhtml+xml,application\
+                            /xml;q=0.9,image/avif,image/webp,image/apng,*/*;\
+                                q=0.8,application/signed-exchange;v=b3;q=0.7',
+                        'Accept-Encoding': 'gzip, deflate, br, zstd',
+                        'Accept-Language': 'en-US,en;q=0.9',
+                        'Cache-Control': "no-cache",
+                        'Pragma': "no-cache",
+                        'Priority': "u=0, i",
+                        'Sec-Ch-Ua': '"Chromium";v="134", \
+                            "Not:A-Brand";v="24","Google Chrome";v="134"',
+                        'Sec-Ch-Mobile': "?0",
+                        'Sec-Ch-Ua-Platform': '"macOS"',
+                        'Sec-Fetch-Dest': "document",
+                        'Sec-Fetch-Mode': "navigate",
+                        'Sec-Fetch-User': "?1",
+                        'Upgrade-Insecure-Requests': '1',
+                    }
+                }
+            )
+
+        sb.driver.execute_cdp_cmd(
+                "Network.setUserAgentOverride",
+                {
+                    "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X \
+                        10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) \
+                            Chrome/134.0.0.0 Safari/537.36"
+                },
+            )
+
+        sb.driver.execute_script("Object.defineProperty(navigator, \
+                                 'webdriver',{get: () => undefined})")
+
+        sb.open(f"{stock_website}")
+        sb.sleep(2)
+
+        sb.wait_for_element_present("span[class='bzi-bars']")
+        sb.click("span[class='bzi-bars']")
+        sb.sleep(1)
+        sb.click("label[for='fltrAll']")
+        sb.sleep(1)
+        sb.click("div.actions.text-right.text-caps > button.btn--primary")
+        sb.sleep(1)
+
+        sb.wait_for_element_present("input[name='date']")
+        sb.click("input[name='date']")
+
+        sb.sleep(1)
+        # sb.wait_for_element_present("input[name='date']")
+        # sb.click("input[name='date']")
+        sb.sleep(1)
+
+        first_calendar_header = sb.find_element(
+            "div[class='mx-calendar-header']").text
+        print(f"First calendar header: {first_calendar_header}")
+
+        if today_month_year != first_calendar_header:
+            sb.click("button[class='mx-btn mx-btn-text mx-btn-icon-left']")
+
+        sb.sleep(2)
+
+        print("Clicking Date")
+        sb.wait_for_element_present(f"td[title = '{today_date}']")
+        today_date_button = sb.find_element(f"td[title = '{today_date}']")
+        today_date_button.click()
+
+        sb.sleep(1)
+
+        sb.wait_for_element_present("select[name='perPageSelect']")
+        sb.click("select[name='perPageSelect']")
+        sb.sleep(1)
+        sb.click("option[value='-1']")
+        sb.sleep(2)
+
+        raw_html = sb.get_page_source()
+        soup = BeautifulSoup(raw_html, 'html5lib')
+        table = soup.find('table', id='vgt-table')
+
+        headers = []
+        for th in table.find('thead').find('tr').find_all('th'):
+            span = th.find('span')
+            if span:
+                headers.append(span.get_text(strip=True))
+            else:
+                headers.append(th.get_text(strip=True))
+        # Rename the first header to 'Line Number'
+        headers[0] = 'No'
+        # headers
+
+        # Extract rows from the tbody
+        rows = []
+        for row in table.find('tbody').find_all('tr'):
+            # Find all th and td elements in the row
+            cells = row.find_all(['th', 'td'])
+            # Extract text from each cell and strip whitespace
+            row_data = [cell.get_text(strip=True) for cell in cells]
+            rows.append(row_data)
+
+        # Create a DataFrame
+        df = pd.DataFrame(rows, columns=headers)
+        raw_df = df[['Kode Saham', 'Tanggal Perdagangan Terakhir',
+                     'Open Price', 'Penutupan', 'Terendah', 'Tertinggi',
+                     'Selisih', 'Volume', 'Nilai', 'Frekuensi',
+                     'Non Regular Volume', 'Non Regular Value',
+                     'Non Regular Frequency', 'Listed Shares',
+                     'Tradeble Shares', 'Offer', 'Offer Volume',
+                     'Bid', 'Bid Volume']].copy()
+
+        month_map = {'Januari': '01', 'Februari': '02', 'Maret': '03',
+                     'April': '04', 'Mei': '05', 'Juni': '06',
+                     'Juli': '07', 'Agustus': '08', 'September': '9',
+                     'Oktober': '10', 'November': '11', 'Desember': '12'}
+
+        for month_str, n_month in month_map.items():
+            raw_df['Tanggal Perdagangan Terakhir'] = raw_df[
+                'Tanggal Perdagangan Terakhir']\
+                    .str.replace(month_str, str(n_month))
+            raw_df['Tanggal Perdagangan Terakhir'] = raw_df[
+                'Tanggal Perdagangan Terakhir']\
+                .str.replace(" ", "-")
+
+        raw_df['Volume'] = raw_df['Volume'].str.replace(".", "").astype(int)
+        raw_df['Volume'] = raw_df['Volume'] / 100
+        raw_df['Terendah'] = raw_df['Terendah'].str.replace(".", "")\
+            .astype(int)
+        raw_df['Tertinggi'] = raw_df['Tertinggi'].str.replace(".", "")\
+            .astype(int)
+        raw_df['Non Regular Volume'] = raw_df['Non Regular Volume']\
+            .str.replace(".", "").astype(int)
+        raw_df['Non Regular Volume'] = raw_df['Non Regular Volume'] / 100
+        raw_df['Non Regular Value'] = raw_df['Non Regular Value']\
+            .str.replace(".", "").astype(int)
+        raw_df['Nilai'] = raw_df['Nilai'].str.replace(".", "").astype(int)
+        raw_df['Non Regular Frequency'] = raw_df['Non Regular Frequency']\
+            .str.replace(".", "").astype(int)
+        raw_df['Penutupan'] = raw_df['Penutupan'].str.replace(".", "")\
+            .astype(int)
+        raw_df['Offer Volume'] = raw_df['Offer Volume'].str.replace(".", "")\
+            .astype(int)
+        raw_df['Offer Volume'] = raw_df['Offer Volume'] / 100
+        raw_df['Bid Volume'] = raw_df['Bid Volume'].str.replace(".", "")\
+            .astype(int)
+        raw_df['Bid Volume'] = raw_df['Bid Volume'] / 100
+        raw_df.rename(
+            {'Kode Saham': 'symbol', 'Tanggal Perdagangan Terakhir': 'date'},
+            axis=1, inplace=True)
+        raw_df['value_ratio'] = raw_df['Non Regular Value'] / raw_df['Nilai']
+        raw_df['avg_price'] = raw_df['Nilai'] / raw_df['Volume'] / 100
+        raw_df['avg_nonreg_price'] = (raw_df['Non Regular Value']
+                                      / raw_df['Non Regular Volume'] / 100)
+        raw_df['avg_nonreg_diff_tertinggi'] = ((raw_df['avg_nonreg_price'] -
+                                                raw_df['Tertinggi']) /
+                                               raw_df['Tertinggi'] * 100)
+        raw_df['link'] = raw_df['symbol']\
+            .apply(lambda x: f"{website}/stock_detail/{x}")
+
+        filtered_df = raw_df[
+            (raw_df['Non Regular Value'] > raw_df['Nilai']) &
+            (raw_df['Non Regular Frequency'] <= 5) &
+            (raw_df['value_ratio'] >= 3) &
+            (raw_df['Penutupan'] >= 60) &
+            (raw_df['Offer Volume'] > 0) &
+            (raw_df['Bid Volume'] > 0)]\
+            .sort_values('value_ratio', ascending=False)\
+            .reset_index(drop=True)
+
+        high_nonreg_price_df = raw_df[
+            (raw_df['avg_nonreg_diff_tertinggi'] >= 1) &
+            (raw_df['value_ratio'] >= 1) &
+            (raw_df['Penutupan'] >= 60) &
+            (raw_df['Offer Volume'] > 0) &
+            (raw_df['Bid Volume'] > 0)]\
+            .sort_values('avg_nonreg_diff_tertinggi', ascending=False)\
+            .reset_index(drop=True)
+
+        sb.open(website)
+        sb.click('[href*="accounts/login"]')
+        sb.type('[name="login"]', f"{site_email}")
+        sb.type('[name="password"]', f"{site_password}")
+        sb.click('button[type*="submit"]')
+        sb.sleep(2)
+
+        final_high_nonreg_price_df = pd.concat([
+            get_individual_stock(
+                sb=sb, row=row)
+            for index, row in high_nonreg_price_df.iterrows()
+        ], ignore_index=True)
+
+        final_filtered_df = pd.concat([
+            get_individual_stock(
+                sb=sb, row=row)
+            for index, row in filtered_df.iterrows()
+        ], ignore_index=True)
+
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+TARGET_CHAT_ID = "1415309056"
+bot = telegram.Bot(token=BOT_TOKEN)
+
+
+async def send_all_nonreg_messages(df, bot, TARGET_CHAT_ID):
+    for index, row in df.iterrows():
+        try:
+            print(f"Processing {type} row {index}...")
+            await send_non_reg_message(
+                row=row,
+                bot=bot,
+                TARGET_CHAT_ID=TARGET_CHAT_ID
+            )
+            await asyncio.sleep(1)  # Add delay between messages
+        except Exception as e:
+            print(f"❌ Failed for {type} row {index}: {e}")
+
+
+async def main():
+    # 1. Send daily summary
+    await send_nonreg_summary_message(
+        df=final_high_nonreg_price_df,
+        bot=bot,
+        type='higher price',
+        TARGET_CHAT_ID=TARGET_CHAT_ID
+    )
+
+    # 2. Send daily individual messages
+    await send_all_nonreg_messages(final_high_nonreg_price_df,
+                                   bot, TARGET_CHAT_ID)
+
+    # 3. Send cumulative summary
+    await send_nonreg_summary_message(
+        df=final_filtered_df,
+        bot=bot,
+        type='non regular',
+        TARGET_CHAT_ID=TARGET_CHAT_ID
+    )
+
+    # 4. Send cumulative individual messages
+    await send_all_nonreg_messages(final_filtered_df, bot,
+                                   TARGET_CHAT_ID)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    private_key_id = os.environ['SA_PRIVKEY_ID']
+    sa_client_email = os.environ['SA_CLIENTMAIL']
+    sa_client_x509_url = os.environ['SA_CLIENT_X509_URL']
+
+    private_key = os.environ['SA_PRIVKEY']
+
+    private_key = private_key.replace('\\n', '\n')
+    full_private_key = f"-----BEGIN PRIVATE KEY-----\n"\
+                       f"{private_key}\n-----END PRIVATE KEY-----\n"
+
+    service_account_dict = {
+        "type": "service_account",
+        "project_id": "keterbukaan-informasi-idx",
+        "private_key_id": private_key_id,
+        "private_key": full_private_key,
+        "client_email": sa_client_email,
+        "client_id": "116805150468350492730",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url":
+        "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": sa_client_x509_url,
+        "universe_domain": "googleapis.com"
+    }
+
+    scope = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/spreadsheets'
+    ]
+
+    gauth = GoogleAuth()
+
+    try:
+        gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            service_account_dict, scope
+        )
+    except Exception as e:
+        print(f"Error loading credentials from dictionary: {e}")
+        # Handle error appropriately, maybe exit
+        exit(1)
+
+    creds = gauth.credentials
+    gc = None
+    spreadsheet = None
+    worksheet = None
+    try:
+        gc = gspread.authorize(creds)
+        print("Google Sheets client (gspread) initialized successfully.")
+
+        sheet_key = "1hZYjUl_ADkBgziBg6QALp7oOBPAr1pbTS9WVP_m4uKI"
+        spreadsheet = gc.open_by_key(sheet_key)
+
+        print(f"Successfully opened spreadsheet: '{spreadsheet.title}'")
+
+    except gspread.exceptions.SpreadsheetNotFound:
+        print("Error: Spreadsheet not found. \n"
+              "1. Check if the name/key/URL is correct.\n")
+        # Decide if you want to exit or continue without sheet access
+        exit(1)
+    except gspread.exceptions.APIError as e:
+        print(f"Google Sheets API Error: {e}")
+        exit(1)
+    except Exception as e:
+        # Catch other potential errors during gspread initialization/opening
+        print(f"An error occurred during Google Sheets setup: {e}")
+        exit(1)
+
+    print("Updating Google Sheet..")
+    export_to_sheets(spreadsheet=spreadsheet, sheet_name='Higher Price',
+                     df=final_high_nonreg_price_df, mode='a')
+
+    export_to_sheets(spreadsheet=spreadsheet, sheet_name='Non Regular',
+                     df=final_filtered_df, mode='a')
--- utils/telegram_utils.py
+++ utils/telegram_utils.py
@@ -30,7 +30,7 @@ async def send_high_level_summary_message(df, bot, type, TARGET_CHAT_ID):
     file_handles = []
     media_group = []
 
-    message_str = generate_high_level_summary_str(df, type)
+    msg_str = generate_high_level_summary_str(df, type)
 
     date = df.date.unique()[0]
     nr_path = f"screenshot/{date}_nr_{type}.png"
@@ -56,7 +56,7 @@ async def send_high_level_summary_message(df, bot, type, TARGET_CHAT_ID):
             media = InputMediaPhoto(
                 media=InputFile(file, filename=os.path.basename(abs_path),
                                 attach=True),
-                caption=message_str if idx == 0 else None,
+                caption=msg_str if idx == 0 else None,
                 parse_mode='HTML' if idx == 0 else None
             )
             media_group.append(media)
@@ -80,59 +80,59 @@ async def send_high_level_summary_message(df, bot, type, TARGET_CHAT_ID):
 
 
 def generate_daily_summary_str(row, type):
-    message_str = f"<a href = '{row['link']}'>{row['symbol']}</a>"
-    message_str += '\n\n'
-    message_str += f"<b>Price</b>: {row['price']}"
-    message_str += '\n'
+    msg_str = f"<a href = '{row['link']}'>{row['symbol']}</a>"
+    msg_str += '\n\n'
+    msg_str += f"<b>Price</b>: {row['price']}"
+    msg_str += '\n'
     if type == 'daily':
-        message_str += f'<b>1d Price Change</b>: {row["%1d"]}%'
+        msg_str += f'<b>1d Price Change</b>: {row["%1d"]}%'
     else:
-        message_str += f'<b>3d Price Change</b>: {row["%3d"]}%'
-        message_str += '\n'
-        message_str += f'<b>5d Price Change</b>: {row["%5d"]}%'
-        message_str += '\n'
-        message_str += f'<b>10d Price Change</b>: {row["%10d"]}%'
-        message_str += '\n'
-        message_str += f'<b>20d Price Change</b>: {row["%20d"]}%'
-    message_str += '\n'
-    message_str += f'<b>pinky</b>: {row["pinky"]}'
-    message_str += '\n'
-    message_str += f'<b>crossing</b>: {row["crossing"]}'
-    message_str += '\n'
-    message_str += f'<b>unusual</b>: {row["unusual"]}'
-    message_str += '\n'
-    message_str += f'<b>likuid</b>: {row["likuid"]}'
-    message_str += '\n\n'
-    message_str += f"<b>{row['method'].upper()}</b>"
-    message_str += '\n'
+        msg_str += f'<b>3d Price Change</b>: {row["%3d"]}%'
+        msg_str += '\n'
+        msg_str += f'<b>5d Price Change</b>: {row["%5d"]}%'
+        msg_str += '\n'
+        msg_str += f'<b>10d Price Change</b>: {row["%10d"]}%'
+        msg_str += '\n'
+        msg_str += f'<b>20d Price Change</b>: {row["%20d"]}%'
+    msg_str += '\n'
+    msg_str += f'<b>pinky</b>: {row["pinky"]}'
+    msg_str += '\n'
+    msg_str += f'<b>crossing</b>: {row["crossing"]}'
+    msg_str += '\n'
+    msg_str += f'<b>unusual</b>: {row["unusual"]}'
+    msg_str += '\n'
+    msg_str += f'<b>likuid</b>: {row["likuid"]}'
+    msg_str += '\n\n'
+    msg_str += f"<b>{row['method'].upper()}</b>"
+    msg_str += '\n'
     if type == 'daily':
-        message_str += f"<b>d-0</b>: {row['dn-0']}%"
-        message_str += '\n'
-        message_str += f"<b>d-1</b>: {row['dn-1']}%"
-        message_str += '\n'
-        message_str += f"<b>d-2</b>: {row['dn-2']}%"
-        message_str += '\n'
-        message_str += f"<b>d-3</b>: {row['dn-3']}%"
-        message_str += '\n'
-        message_str += f"<b>d-4</b>: {row['dn-4']}%"
-        message_str += '\n\n'
-        message_str += f"<b>w-1</b>: {row['wn-1']}%"
-        message_str += '\n'
-        message_str += f"<b>w-2</b>: {row['wn-2']}%"
-        message_str += '\n'
-        message_str += f"<b>w-3</b>: {row['wn-3']}%"
-        message_str += '\n'
-        message_str += f"<b>w-4</b>: {row['wn-4']}%"
+        msg_str += f"<b>d-0</b>: {row['dn-0']}%"
+        msg_str += '\n'
+        msg_str += f"<b>d-1</b>: {row['dn-1']}%"
+        msg_str += '\n'
+        msg_str += f"<b>d-2</b>: {row['dn-2']}%"
+        msg_str += '\n'
+        msg_str += f"<b>d-3</b>: {row['dn-3']}%"
+        msg_str += '\n'
+        msg_str += f"<b>d-4</b>: {row['dn-4']}%"
+        msg_str += '\n\n'
+        msg_str += f"<b>w-1</b>: {row['wn-1']}%"
+        msg_str += '\n'
+        msg_str += f"<b>w-2</b>: {row['wn-2']}%"
+        msg_str += '\n'
+        msg_str += f"<b>w-3</b>: {row['wn-3']}%"
+        msg_str += '\n'
+        msg_str += f"<b>w-4</b>: {row['wn-4']}%"
     else:
-        message_str += f"<b>cn-3</b>: {row['cn-3']}%"
-        message_str += '\n'
-        message_str += f"<b>cn-5</b>: {row['cn-5']}%"
-        message_str += '\n'
-        message_str += f"<b>cn-10</b>: {row['cn-10']}%"
-        message_str += '\n'
-        message_str += f"<b>cn-20</b>: {row['cn-20']}%"
-        message_str += '\n'
-    return message_str
+        msg_str += f"<b>cn-3</b>: {row['cn-3']}%"
+        msg_str += '\n'
+        msg_str += f"<b>cn-5</b>: {row['cn-5']}%"
+        msg_str += '\n'
+        msg_str += f"<b>cn-10</b>: {row['cn-10']}%"
+        msg_str += '\n'
+        msg_str += f"<b>cn-20</b>: {row['cn-20']}%"
+        msg_str += '\n'
+    return msg_str
 
 
 # Define as an async function
@@ -141,7 +141,117 @@ async def send_daily_message(row, bot, type, TARGET_CHAT_ID):
     file_handles = []
     media_group = []
 
-    message_str = generate_daily_summary_str(row, type)
+    msg_str = generate_daily_summary_str(row, type)
+
+    try:
+        for idx, file_path in enumerate(row['ss_directory']):
+            # Step 1: Validate absolute path
+            abs_path = os.path.abspath(file_path)
+            if not os.path.exists(abs_path):
+                print(f"🚫 File not found: {abs_path}")
+                continue
+
+            # Step 2: Normalize path (critical for Windows)
+            abs_path = abs_path.replace("\\", "/")  # Force forward slashes
+
+            # Step 3: Open file and retain handle
+            file = open(abs_path, "rb")
+            file_handles.append(file)  # Keep track of open files
+
+            # Step 4: Create media with HTML caption parsing
+            media = InputMediaPhoto(
+                media=InputFile(file, filename=os.path.basename(abs_path),
+                                attach=True),
+                caption=msg_str if idx == 0 else None,
+                parse_mode='HTML' if idx == 0 else None
+            )
+            media_group.append(media)
+            print(f"✅ Added {abs_path} to media group")
+
+        # Step 5: Send media group (await inside async function)
+        await bot.send_media_group(chat_id=TARGET_CHAT_ID, media=media_group)
+        print("Media group sent successfully!")
+
+    except BadRequest as e:
+        print(f"💥 Telegram API Error: {e.message}")
+        print("Debug Checklist:")
+        print("1. File paths: ",
+              [os.path.abspath(p) for p in row['ss_directory']])
+        print("2. File sizes: ",
+              [f"{os.path.getsize(p)/1e6:.2f} MB"
+               for p in row['ss_directory']])
+
+    finally:
+        # Step 6: Cleanup - close all files
+        for f in file_handles:
+            f.close()
+
+
+def generate_non_regular_summary_str(df, type):
+    if type == 'non regular':
+        summary_str = f'<b>{df.date.unique()[0]}'\
+            ' - TOP NON REGULAR TRANSACTION VALUE</b>'
+    if type == 'higher price':
+        summary_str = f'<b>{df.date.unique()[0]}'\
+            ' - TOP NON REGULAR HIGHER PRICE</b>'
+
+    summary_str += '\n\n'
+    all_stock = df.symbol.to_list()
+    summary_str += ', '.join(all_stock)
+
+    return summary_str
+
+
+async def send_nonreg_summary_message(df, bot, type, TARGET_CHAT_ID):
+    # Store open file handles to prevent premature closing
+    msg_str = generate_non_regular_summary_str(df, type)
+    try:
+        await bot.send_message(chat_id=TARGET_CHAT_ID,
+                               text=msg_str,
+                               parse_mode='HTML')
+        print("Media group sent successfully!")
+
+    except BadRequest as e:
+        print(f"💥 Telegram API Error: {e.message}")
+
+
+def generate_non_reg_str(row):
+    msg_str = f"<a href = '{row['link']}'>{row['symbol']}</a>"
+    msg_str += '\n\n'
+    msg_str += f"<b>Last Price</b>: {row['Penutupan']}"
+    msg_str += '\n'
+    msg_str += f"<b>Highest Price</b>: {row['Tertinggi']}"
+    msg_str += '\n'
+    msg_str += (
+        f"<b>Avg Non Reg Price</b>: {row['avg_nonreg_price']:.0f} "
+        f"({'+' if row['avg_nonreg_diff_tertinggi'] > 0 else ''}"
+        f"{row['avg_nonreg_diff_tertinggi']:.1f}%)"
+    )
+    msg_str += '\n\n'
+    msg_str += f"<b>Volume</b>: {row['Volume']:,.0f} Lot"
+    msg_str += '\n'
+    msg_str += f"<b>Value</b>: Rp{row['Nilai']:,}"
+    msg_str += '\n'
+    msg_str += f"<b>Frekuensi</b>: {row['Frekuensi']}"
+    msg_str += '\n\n'
+    msg_str += f"<b>Non Reg Volume</b>: {row['Non Regular Volume']:,.0f}"\
+        " Lot"
+    msg_str += '\n'
+    msg_str += f"<b>Non Reg Value</b>: Rp{row['Non Regular Value']:,}"
+    msg_str += '\n'
+    msg_str += f"<b>Non Reg Frekuensi</b>: {row['Non Regular Frequency']:,}"
+    msg_str += '\n'
+    msg_str += f"<b>Non Reg Value Ratio</b>: {row['value_ratio']:,.1f}x"
+
+    return msg_str
+
+
+async def send_non_reg_message(row, bot, TARGET_CHAT_ID):
+    # Store open file handles to prevent premature closing
+    file_handles = []
+    media_group = []
+
+    message_str = generate_non_reg_str(row)
 
     try:
         for idx, file_path in enumerate(row['ss_directory']):
